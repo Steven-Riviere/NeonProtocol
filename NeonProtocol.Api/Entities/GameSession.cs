2391c7f255c1879f3584a8a9dd04b00836073f74
Steven Rivière <stevensteriviere@gmail.com>
2026-07-05 23:29:42 +0200

    feat : add core domain entities (Player, GameSession)

---
+++ NeonProtocol.Api/Entities/GameSession.cs
@@ -0,0 +1,16 @@
+namespace NeonProtocol.Api.Entities;
+
+public class GameSession
+{
+    public int Id { get; set; }
+    public int PlayerId { get; set; }  
+    public Player Player { get; set; } = null!;
+
+    public int Score { get; set; }
+    public int Kills { get; set; }
+    public int LevelReached { get; set; }
+    public int DamageTaken { get; set; }
+    public int SurvivalTime { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ NeonProtocol.Api/Entities/Player.cs
@@ -0,0 +1,9 @@
+namespace NeonProtocol.Api.Entities;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Pseudo { get; set; } = string.Empty;
+
+    public List<GameSession> GameSessions { get; set; } = new ();
+}
